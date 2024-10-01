04926552f3fbaa6d92f1aa4d87fd94b4a7e8594a
zengzilin <zengzilin@users.noreply.github.com>
2024-10-01 13:58:31 +0800

    Update Books.tsx

--- components/Books.tsx
+++ components/Books.tsx
@@ -1,19 +1,12 @@
 import Link from 'next/link';
 
-function MyComponent() {
+export default function MyComponent() {
   return (
     <div>
       {/* Next.js 的 Link 组件用于应用内部导航 */}
-      <Link href="/about">
-        <a>关于我们</a>
+      <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+        <a>访问新AI书籍摘要网站</a>
       </Link>
-
-      {/* 使用 HTML 的 <a> 标签指向外部网址并在新标签页中打开 */}
-      <a href="https://https://ai-books-summary.com/" target="_blank" rel="noopener noreferrer">
-        访问AI书籍摘要网站
-      </a>
     </div>
   );
 }
-
-export default MyComponent;
