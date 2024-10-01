41145a710fa9221237968b2da936cdc10cb27ecc
zengzilin <zengzilin@users.noreply.github.com>
2024-10-01 14:02:29 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import Main from './Main'
 
-export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+import { allBlogs } from 'contentlayer/generated'
+
+import Main from './Main' 
+import MyComponent from '@/components/Books' 
+
+export default async function Page() { 
+  const sortedPosts = sortPosts(allBlogs) 
+  const posts = allCoreContent(sortedPosts) 
+  return (
+    <>
+      <MyComponent />
+      <Main posts={posts} />    
+    </>
+  ) 
+     
 }
